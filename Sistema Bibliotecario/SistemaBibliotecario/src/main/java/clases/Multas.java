5827ce70be8d4459ae242c2aeffafd0f9e637339
Villawolf <juanLobito@gmail.com>
2024-06-24 22:12:44 -0500

    usuario, multas y prestamos

--- Sistema Bibliotecario/SistemaBibliotecario/src/main/java/clases/Multas.java
+++ Sistema Bibliotecario/SistemaBibliotecario/src/main/java/clases/Multas.java
@@ -9,5 +9,61 @@ package clases;
  * @author javm_
  */
 public class Multas {
+    private int multaId;
+    private int prestamoId;
+    private double monto;
+    private double pagado;
+    private int administradorId;
+
+    public Multas(double monto, double pagado) {
+        this.monto = monto;
+        this.pagado = pagado;
+    }
+
+    public int getMultaId() {
+        return multaId;
+    }
+
+    public void setMultaId(int multaId) {
+        this.multaId = multaId;
+    }
+
+    public int getPrestamoId() {
+        return prestamoId;
+    }
+
+    public void setPrestamoId(int prestamoId) {
+        this.prestamoId = prestamoId;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public double getPagado() {
+        return pagado;
+    }
+
+    public void setPagado(double pagado) {
+        this.pagado = pagado;
+    }
+
+    public int getAdministradorId() {
+        return administradorId;
+    }
+
+    public void setAdministradorId(int administradorId) {
+        this.administradorId = administradorId;
+    }
+
+    @Override
+    public String toString() {
+        return "Multas: " + "multaId=" + multaId + ", prestamoId=" + prestamoId + ", monto=" + monto + ", pagado=" + pagado + ", administradorId=" + administradorId + '"';
+    }
+    
     
 }
--- Sistema Bibliotecario/SistemaBibliotecario/src/main/java/clases/Usuario.java
+++ Sistema Bibliotecario/SistemaBibliotecario/src/main/java/clases/Usuario.java
@@ -4,10 +4,79 @@
  */
 package clases;
 
+import java.util.Calendar;
+
 /**
  *
  * @author javm_
  */
 public class Usuario {
+    private int usuarioId;
+    private String nombre;
+    private String email;
+    private String contraseña;
+    private Calendar fechaRegistro;
+    private int administradorId;
+
+    public Usuario(String nombre, String email, String contraseña, Calendar fechaRegistro) {
+        this.nombre = nombre;
+        this.email = email;
+        this.contraseña = contraseña;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public Calendar getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Calendar fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getAdministradorId() {
+        return administradorId;
+    }
+
+    public void setAdministradorId(int administradorId) {
+        this.administradorId = administradorId;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario: " + "usuarioId=" + usuarioId + ", nombre=" + nombre + ", email=" + email + ", contrase\u00f1a=" + contraseña + ", fechaRegistro=" + fechaRegistro + ", administradorId=" + administradorId + ':';
+    }
+    
     
 }
